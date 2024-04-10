d97aa63350f38e81b7557f67717458d07ad674aa
Lydia Yuan <lydiayuan99@gmail.com>
2024-04-10 10:12:54 -0400

    add: blank views project

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# bill-smart
+# Bill Smart
+
+## Setup
+
+```shell
+chmod +x .githooks/pre-commit
+git config --get core.hooksPath
+``` 
+
+## Code Style References
+
+- [tivi's configuration](https://github.com/chrisbanes/tivi/tree/main?tab=readme-ov-file#code-style)
+- [Duckduckgo's configuration](https://github.com/duckduckgo/Android/blob/develop/.githooks/pre-commit)
---
+++ build.gradle.kts
@@ -0,0 +1,26 @@
+import com.android.build.gradle.AppPlugin
+import com.android.build.gradle.LibraryPlugin
+import com.diffplug.gradle.spotless.SpotlessExtension
+
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    alias(libs.plugins.androidApplication) apply false
+    alias(libs.plugins.jetbrainsKotlinAndroid) apply false
+    alias(libs.plugins.spotless).apply(false)
+    id("org.jmailen.kotlinter") version "4.3.0" apply false
+}
+
+
+subprojects {
+    plugins.matching { anyPlugin -> anyPlugin is AppPlugin || anyPlugin is LibraryPlugin }.whenPluginAdded {
+        apply(plugin = libs.plugins.spotless.get().pluginId)
+        extensions.configure<SpotlessExtension> {
+            kotlin {
+                target("**/*.kt")
+                targetExclude("${layout.buildDirectory}/**/*.kt")
+                ktlint()
+                    .setEditorConfigPath("${project.rootDir}/.editorconfig")
+            }
+        }
+    }
+}
